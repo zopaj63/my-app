c7ea2019d93b9ca36fa304e5ada44daa89783be3
zopaj63 <zoran.pajnic@gmail.com>
2021-09-19 20:03:05 +0200

    add index.js in components map

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as CoinToss } from "./CoinToss";
+export { default as CurrentDate } from "./CurrentDate";
+export { default as UniqueId } from "./UniqueId";
+export { default as UserInfo } from "./UserInfo";
