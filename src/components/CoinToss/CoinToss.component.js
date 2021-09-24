839201533cc4eabe599823990a363b5b4c127908
zopaj63 <zoran.pajnic@gmail.com>
2021-09-24 23:46:32 +0200

    add CoinToss images

--- src/components/CoinToss/CoinToss.component.js
+++ src/components/CoinToss/CoinToss.component.js
@@ -1,9 +1,24 @@
+import PropTypes from "prop-types";
+
+import headsimage from "./images/headsimage.png"
+import tailsimage from "./images/tailsimage.png"
+
 export default function CoinTossComponent({ isHeads, tossCoin }) {
 
     return (
         <span>
             The result of the coin toss is < span style={{ color: "lightGreen" }}></span > {isHeads && <span style={{ color: "lightGreen" }}>"heads"</span>} {!isHeads && <span style={{ color: "lightGreen" }}>"tails"</span>} !
+
             <button onClick={tossCoin}> Toos the coin</button >
+
+            {isHeads && <img src={headsimage} alt="headscoin" className="coin" />}
+            {!isHeads && <img src={tailsimage} alt="tailscoin" className="coin" />}
+
         </span >
     );
 }
+
+CoinTossComponent.propTypes = {
+    isHeads: PropTypes.bool,
+    tossCoin: PropTypes.func,
+};
